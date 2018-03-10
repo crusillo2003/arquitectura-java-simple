c1826153c187769234cbc37a6be3a8168d2af0de
Raúl González <raulegleza@gmail.com>
2018-03-10 04:41:22 -0600

    correccion de poms para forzar a usar jdk8 y agregando proyecto de usuarios servicio

---
+++ UsuariosServicios/src/main/java/mx/rafex/blog/back/servicios/App.java
@@ -0,0 +1,13 @@
+package mx.rafex.blog.back.servicios;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
