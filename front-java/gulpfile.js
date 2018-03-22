b2a12e5dff776a5263778b96c8e49d68b00f2b12
Raúl González <raulegleza@gmail.com>
2018-03-21 20:40:38 -0600

    funcionando arquitectura front, pugjs, reactjs

--- front-java/gulpfile.js
+++ front-java/gulpfile.js
@@ -4,10 +4,14 @@ var source = require('vinyl-source-stream');
 var gutil = require('gulp-util');
 var babelify = require('babelify');
 var connect = require('gulp-connect');
+var pump = require('pump');
+var pug = require('gulp-pug');
+var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
 
 var dependencies = ['react', 'react-dom'];
 
-gulp.task('connect', function() {
+gulp.task('server', function() {
     connect.server({
         root: ['./web/'],
         port: 3000,
@@ -15,6 +19,48 @@ gulp.task('connect', function() {
         livereload: true
     });
 });
+
+gulp.task('views', function buildHTML() {
+  return gulp.src('./views/*.pug')
+  .pipe(pug({
+      doctype: 'html',
+      pretty: true
+  }))
+  .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('./dist/js/*.js'),
+        uglify(),
+        gulp.dest('./web/js/')
+    ],
+    cb
+  );
+});
+
+gulp.task('minify', function() {
+  return gulp.src('./dist/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./web/'));
+});
+
+gulp.task('scripts', function () {
+    bundleApp(false);
+});
+
+gulp.task('deploy', function (){
+	bundleApp(true);
+});
+
+gulp.task('watch', function () {
+	gulp.watch(['./app/*.js'], ['scripts']);
+    gulp.watch(['./views/*.pug'], ['views']);
+    gulp.watch(['./html/*.html'], ['minify']);
+    gulp.watch(['./dist/js/*.js'], ['compress']);
+});
+
+gulp.task('default', ['scripts','views','minify','compress','watch','server']);
 
 var scriptsCount = 0;
 
@@ -33,7 +79,7 @@ function bundleApp(isProduction) {
         .bundle()
         .on('error', gutil.log)
         .pipe(source('vendors.js'))
-        .pipe(gulp.dest('./web/js/'));
+        .pipe(gulp.dest('./dist/js/'));
     }
     if (!isProduction) {
         dependencies.forEach(function(dep) {
@@ -46,5 +92,5 @@ function bundleApp(isProduction) {
     .bundle()
     .on('error', gutil.log)
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./web/js/'));
+    .pipe(gulp.dest('./dist/js/'));
 }
